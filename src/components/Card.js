a2186bb6583341a675561f9c74e4c8e13d41deef
Bryce Dugger <bryce.dugger@outlook.com>
2020-01-26 01:15:57 -0800

    heroku deploy

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
-import React from "react";
-import "../App.css";
+// import React from "react";
+// import "../App.css";
 
-function Card (props) {
-    return (
-        <div className="col-3 mb-4">
-            <div className="card">
-                <img 
-                    className="card-img-top"
-                    src={props.image.img}
-                    //calls cardClicked function on Main.js
-                    //passes id through function to be compared to
-                    //this.state in Main.js
-                    //brackets dilliniate javascript of function
-                    onClick={() => props.cardClicked(props.image.id)}
-                    id={props.image.id}
-                />
-            </div>
-        </div>
-    );
-}
+// function Card (props) {
+//     return (
+//         <div className="col-3 mb-4">
+//             <div className="card">
+//                 <img 
+//                     className="card-img-top"
+//                     src={props.image.img}
+//                     //calls cardClicked function on Main.js
+//                     //passes id through function to be compared to
+//                     //this.state in Main.js
+//                     //brackets dilliniate javascript of function
+//                     onClick={() => props.cardClicked(props.image.id)}
+//                     id={props.image.id}
+//                 />
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Card;
+// export default Card;
